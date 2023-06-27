d5c7d85b811a43f2b6b7b384a628bf51ed42cbbe
Jan Simson <git@simson.io>
2023-06-27 12:28:28 +0200

    Generate links to admin UI and api-docs

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,8 @@ async function init() {
   app.listen(port, () => {
     /* eslint-disable no-console */
     console.log(`Listening on: http://localhost:${port}`);
+    console.log(`Admin UI at: http://localhost:${port}/admin`);
+    console.log(`API Docs at: http://localhost:${port}/api-docs`);
     /* eslint-enable no-console */
   });
 }
