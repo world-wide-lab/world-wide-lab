5ccc802a71c78feb2edf6e2eca802f074093a09f
Jan Simson <git@simson.io>
2023-07-16 12:35:03 +0200

    Fix bug in sequelize parsing of database URL strings, by manually parsing sqlite URLs

--- packages/wwl/src/db/index.ts
+++ packages/wwl/src/db/index.ts
@@ -1,15 +1,26 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, Options as SequelizeOptions } from 'sequelize';
 
 import { defineModels } from './models';
 import config from '../config';
 import { log_sql } from '../logger'
 
-const sequelize = new Sequelize(
-  config.database.url,
-  {
-    logging: log_sql,
-  }
-);
+const url = config.database.url
+const options: SequelizeOptions = {
+  logging: log_sql,
+}
+
+// There is a bug in handling filepaths with spaces when using sqlite via
+// a URL string. Therefore we manually pass it the database URL for sqlite.
+const isSqlite = url.startsWith('sqlite:')
+if (isSqlite) {
+  // Manually parse URL instead and add it to options
+  options.dialect = 'sqlite'
+  options.storage = url
+    .replace('sqlite://', '')
+    .replace('sqlite:', '')
+}
+const sequelize = isSqlite ?  new Sequelize(options) : new Sequelize(url, options);
+
 defineModels(sequelize);
 
 export default sequelize;
