969ada188e28998944c639680f5fc3b4550609b7
Jan Simson <git@simson.io>
2023-07-27 14:24:28 +0200

    Add working docker integration incl README

--- packages/wwl/docker/build.sh
+++
@@ -1,1 +0,0 @@
-docker buildx build --no-cache -t world-wide-lab/server:latest api --platform=linux/amd64
--- packages/wwl/docker/publish.sh
+++
@@ -1,3 +0,0 @@
-
-docker tag world-wide-lab/server ghcr.io/world-wide-lab/server:latest
-docker push ghcr.io/world-wide-lab/server:latest
