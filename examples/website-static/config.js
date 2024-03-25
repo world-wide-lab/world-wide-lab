8c4dcb1d25cb8a945e40ba4c01813d0a154143a0
Jan Simson <git@simson.io>
2024-03-25 17:01:42 +0100

    [examples] Add an example static website

---
+++ examples/website-static/config.js
@@ -0,0 +1,23 @@
+// There will automatically be a card generated for each study you create here.
+window.studies = [
+  {
+    // Required
+    // The title of your study
+    title: "Example Study",
+    // A short description, ideally getting people excited to take part
+    description:
+      "Briefly describe what your study is about and why people should totally take part in it!",
+    // The URL to your study, this should be the relative path to its index.html file
+    url: "studies/study-jsPsych/index.html",
+
+    // Optional fields
+    // The estimated duration of your study
+    duration: "10 minutes",
+    // An image / logo to display on the card
+    image: "img/example.png",
+    // Whether to show the study within a frame
+    // (this will keep the website navigation visible, but makes debugging the study a bit harder)
+    useFrame: true,
+  },
+  // Just copy over the full object (everything between {}) to add another study
+];
