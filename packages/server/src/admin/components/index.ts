a85b8254d31d6b0b1979d141919820a8ca364a12
Jan Simson <git@simson.io>
2024-04-14 22:51:32 +0200

    [server] Use component loader patterns from official adminjs example

--- packages/server/src/admin/components/index.ts
+++ packages/server/src/admin/components/index.ts
@@ -1,19 +1,27 @@
-import { ComponentLoader } from "adminjs";
+import { ComponentLoader, OverridableComponent } from "adminjs";
 
-const componentLoader = new ComponentLoader();
+import path from 'path';
+import * as url from 'url';
 
-const Components = {
-  Dashboard: componentLoader.add("Dashboard", "./pages/dashboard"),
-  ApiDocsPage: componentLoader.add("ApiDocsPage", "./pages/api-docs"),
-  StudyDownloadAction: componentLoader.add(
+export const componentLoader = new ComponentLoader();
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+export const add = (componentName: string, url: string): string =>
+  componentLoader.add(componentName, path.join(__dirname, url));
+
+export const override = (componentName: OverridableComponent, url: string): string =>
+  componentLoader.override(componentName, path.join(__dirname, url));
+
+export const Components = {
+  Dashboard: add("Dashboard", "./pages/dashboard.tsx"),
+  ApiDocsPage: add("ApiDocsPage", "./pages/api-docs.tsx"),
+  StudyDownloadAction: add(
     "StudyDownloadAction",
-    "./StudyDownloadAction",
+    "./StudyDownloadAction.tsx",
   ),
-  StudyShowAction: componentLoader.add("StudyShowAction", "./StudyShowAction"),
-  ShowJsonProp: componentLoader.add(
+  StudyShowAction: add("StudyShowAction", "./StudyShowAction.tsx"),
+  ShowJsonProp: add(
     "ShowJsonProp",
-    "./properties/ShowJsonProp",
+    "./properties/ShowJsonProp.tsx",
   ),
 };
-
-export { componentLoader, Components };
