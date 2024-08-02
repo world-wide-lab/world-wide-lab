5cb74ff9e211f3ef43a5a848f4104d773652ecf6
Jan Simson <git@simson.io>
2024-08-02 21:47:01 +0200

    [deploy] Remove ECS from AppRunner deployment parts

--- packages/deploy/src/aws/stacks/app-runner.ts
+++ packages/deploy/src/aws/stacks/app-runner.ts
@@ -2,7 +2,6 @@ import merge from "deepmerge";
 import dotenv from "dotenv";
 
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 import * as pulumi from "@pulumi/pulumi";
 
 import type { WwlAwsDeploymentConfig } from "..";
@@ -15,11 +14,6 @@ export class WwlAwsAppRunnerDeployment {
   readonly autoScalingConfig: aws.apprunner.AutoScalingConfigurationVersion;
   readonly vpcConnector: aws.apprunner.VpcConnector;
   readonly appRunnerService: aws.apprunner.Service;
-  readonly loadbalancer: awsx.lb.ApplicationLoadBalancer;
-  readonly cluster: aws.ecs.Cluster;
-  readonly service: awsx.ecs.FargateService;
-  readonly scalingTarget: aws.appautoscaling.Target;
-  readonly scalingPolicy: aws.appautoscaling.Policy;
 
   /**
    * Creates a new static website hosted on AWS.
@@ -155,108 +149,8 @@ export class WwlAwsAppRunnerDeployment {
       },
     });
 
-    // - AWS ECS -
-
-    // An ALB to serve the container endpoint to the internet
-    this.loadbalancer = new awsx.lb.ApplicationLoadBalancer(
-      "wwl-loadbalancer",
-      {},
-    );
-
-    // An ECS cluster to deploy into
-    this.cluster = new aws.ecs.Cluster("wwl-cluster", {});
-
-    // Deploy an ECS Service on Fargate to host the application container
-    this.service = new awsx.ecs.FargateService("wwl-server-service", {
-      cluster: this.cluster.arn,
-      assignPublicIp: true,
-      forceNewDeployment: true,
-      taskDefinitionArgs: {
-        container: {
-          name: "wwl-server",
-          image: "ghcr.io/world-wide-lab/server:latest",
-          cpu: this.config.cpu,
-          memory: this.config.memory,
-          essential: true,
-          portMappings: [
-            {
-              hostPort: this.config.containerPort,
-              containerPort: this.config.containerPort,
-              targetGroup: this.loadbalancer.defaultTargetGroup,
-            },
-          ],
-          environment: [
-            { name: "NODE_ENV", value: "production" },
-            { name: "PORT", value: `${this.config.containerPort}` },
-            { name: "DATABASE_URL", value: this.dbConnectionString },
-            { name: "ADMIN_UI", value: "true" },
-            { name: "USE_AUTHENTICATION", value: "true" },
-            { name: "REPLICATION_ROLE", value: "source" },
-            {
-              name: "ADMIN_AUTH_DEFAULT_EMAIL",
-              value: process.env.WWL_ADMIN_AUTH_DEFAULT_EMAIL,
-            },
-            {
-              name: "ADMIN_AUTH_DEFAULT_PASSWORD",
-              value: process.env.WWL_ADMIN_AUTH_DEFAULT_PASSWORD,
-            },
-            {
-              name: "ADMIN_AUTH_SESSION_SECRET",
-              value: process.env.WWL_ADMIN_AUTH_SESSION_SECRET,
-            },
-            { name: "DEFAULT_API_KEY", value: process.env.WWL_DEFAULT_API_KEY },
-            { name: "DATABASE_CHUNK_SIZE", value: "5000" },
-            { name: "LOGGING_HTTP", value: "false" },
-            { name: "LOGGING_SQL", value: "false" },
-            // set to 'verbose' to show SQL & HTTP logs (if enabled)
-            { name: "LOGGING_LEVEL_CONSOLE", value: "info" },
-          ],
-          logConfiguration: {
-            logDriver: "awslogs",
-            options: {
-              "awslogs-group": "/ecs/wwl-server",
-              "awslogs-region": "us-east-1",
-              "awslogs-stream-prefix": "ecs",
-              "awslogs-create-group": "true",
-            },
-          },
-        },
-      },
-    });
-
-    // Define an Application Auto Scaling Target. This specifies the ECS service we want to scale.
-    this.scalingTarget = new aws.appautoscaling.Target(
-      "app-scaling-target",
-      {
-        maxCapacity: this.config.maxCapacity,
-        minCapacity: this.config.minCapacity,
-        resourceId: pulumi.interpolate`service/${this.cluster.name}/${this.service.service.name}`, // Format is "service/clusterName/serviceName"
-        scalableDimension: "ecs:service:DesiredCount",
-        serviceNamespace: "ecs",
-      },
-      { dependsOn: [this.service] },
-    );
-
-    // Define Application Auto Scaling Policy. This specifies how the Target should be scaled.
-    this.scalingPolicy = new aws.appautoscaling.Policy(
-      "app-scaling-policy",
-      {
-        policyType: "TargetTrackingScaling",
-        resourceId: this.scalingTarget.resourceId,
-        scalableDimension: this.scalingTarget.scalableDimension,
-        serviceNamespace: this.scalingTarget.serviceNamespace,
-        targetTrackingScalingPolicyConfiguration: {
-          targetValue: 80.0,
-          predefinedMetricSpecification: {
-            predefinedMetricType: "ECSServiceAverageCPUUtilization",
-          },
-        },
-      },
-      { dependsOn: [this.scalingTarget] },
-    );
-
     // The URL at which the container's HTTP endpoint will be available
-    this.url = pulumi.interpolate`http://${this.loadbalancer.loadBalancer.dnsName}`;
+    this.url = pulumi.interpolate`http://${this.appRunnerService.serviceUrl}`;
     if (typeof this.url === "string") {
       console.log(`Running at: ${this.url}`);
     }
