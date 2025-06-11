ac7529de1abc7ee2fcd7af54074f04e9bfa11108
Jan Simson <git@simson.io>
2025-06-11 15:22:55 +0200

    Update example AWS deployment

--- packages/deploy/examples/aws-deployment/index.ts
+++ packages/deploy/examples/aws-deployment/index.ts
@@ -1,15 +1,18 @@
 import { WwlAwsDeployment } from "@world-wide-lab/deploy";
 
 new WwlAwsDeployment({
-  secrets: {
-    // It's better if you change these to something matching your use case.
-    dbUsername: "wwlDbUser",
-    wwlAdminAuthDefaultEmail: "admin@world-wide-lab.org",
-    // These should all be long and randomly generated.
-    // See https://www.random.org/strings/?num=5&len=25&digits=on&upperalpha=on&loweralpha=on&format=html&rnd=new
-    dbPassword: "",
-    wwlAdminAuthDefaultPassword: "",
-    wwlAdminAuthSessionSecret: "",
-    wwlDefaultApiKey: "",
-  },
+  // It's better if you change these to something matching your use case.
+  secret_dbUsername: "wwlDbUser",
+  secret_wwlAdminAuthDefaultEmail: "admin@world-wide-lab.org",
+
+  // These should all be long and randomly generated.
+  // See https://www.random.org/strings/?num=5&len=25&digits=on&upperalpha=on&loweralpha=on&format=html&rnd=new
+  secret_dbPassword: "",
+  secret_wwlAdminAuthDefaultPassword: "",
+  secret_wwlAdminAuthSessionSecret: "",
+  secret_wwlDefaultApiKey: "",
+
+  // By default, we protect the database from deletion to protect against data loss.
+  // Set this to false to be able to delete the database.
+  dbDeletionProtection: false,
 });
